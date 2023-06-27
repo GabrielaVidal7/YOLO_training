64a982c8acce1043e403cc8eba66115cb53d429d
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-06-27 15:13:32 -0300

    Dataset Guararema dividido por altitude

--- README.md
+++ README.md
@@ -3,12 +3,15 @@ Repositório criado para armazenar os datasets utilizados além dos arquivos nec
 
 ## Resultados
 A partir do dataset presente, utilizou-se YOLOv7. Os resultados obtidos foram:
-|      Name     |   Train Size  |   Test Size   |   Precision   |    Recall     |      mAP      |
-|  -----------  | ------------- | ------------- | ------------- | ------------- | ------------- |
-|   First_test  |      192      |       53      |     45,1%     |     38,7%     |     35,3%     |
-|  Second_test  |      346      |       86      |      85%      |      86%      |     90,1%     |
+|                Name              |   Train Size  |    Test Size   |   Precision   |    Recall     |      mAP      |
+|  ------------------------------  | ------------- | -------------- | ------------- | ------------- | ------------- |
+|        POLI_2023/First_test      |      192      |        53      |     45,1%     |     38,7%     |     35,3%     |
+|       POLI_2023/Second_test      |      346      |        86      |      85%      |      86%      |     90,1%     |
+|   Guararema_05_06_23/First_test  |      457      |       130      |     29,5%     |     43,6%     |     32,6%     |
 
 ## Folders
-A pasta 'First_test' possui o dataset e resultado do experimento feito com algumas imagens coletadas em 1 dia de vôo pelo campus da USP de São Paulo, na região do Butantã.
+A pasta 'POLI_2023/First_test' possui o dataset e resultado do experimento feito com algumas imagens coletadas em 1 dia de vôo pelo campus da USP de São Paulo, na região do Butantã.
 
-A pasta 'Second_test' possui o dataset e resultado do experimento feito com todas imagens coletadas em 1 dia de vôo pelo campus da USP de São Paulo, na região do Butantã.
+A pasta 'POLI_2023/Second_test' possui o dataset e resultado do experimento feito com todas imagens coletadas em 1 dia de vôo pelo campus da USP de São Paulo, na região do Butantã.
+
+A pasta 'Guararema_05_06_23/First_test' possui o dataset e resultado do experimento feito com algumas imagens coletadas em 1 dia de vôo pela região de Guararema, perto da região da empresa em diversas altitudes, de 60m a 120m.
--- split_data.py
+++ split_data.py
@@ -1,5 +1,60 @@
 import splitfolders
+import shutil
+import os
+
+path_image = "Guararema_05_06_23/Second_test/100m+/class1"
+path_label = "Guararema_05_06_23/Second_test/100m+/class2"
+
+output_image = "Guararema_05_06_23/Second_test/100m+/output"
+output_label = "Guararema_05_06_23/Second_test/100m+/outputlabels"
+
+# Create folder 'class1' and 'class2'
+if not os.path.exists(path_image):
+    os.mkdir(path_image)
+
+if not os.path.exists(path_label):
+    os.mkdir(path_label)
+
+# Move folder 'image' to 'class1' and 'label' to 'class2'
+if os.path.exists(path_image.replace('class1', 'images')):
+    shutil.move(path_image.replace('class1', 'images'), path_image)
+
+if os.path.exists(path_label.replace('class2', 'labels')):
+    shutil.move(path_label.replace('class2', 'labels'), path_label)
 
 # Train: 70%    Val: 20%    Test: 10%s
-splitfolders.ratio('Dataset/class1', output="Dataset/output", seed=1337, ratio=(0.7, 0.2, 0.1))
-splitfolders.ratio('Dataset/class2', output="Dataset/outputlabels", seed=1337, ratio=(0.7, 0.2, 0.1))
+splitfolders.ratio(path_image, output=output_image, seed=1337, ratio=(0.7, 0.2, 0.1))
+splitfolders.ratio(path_label, output=output_label, seed=1337, ratio=(0.7, 0.2, 0.1))
+
+# Move folders to the correct place
+# First: remove folders 'class1' and 'class2'
+# if os.path.exists(path_image):
+#     os.remove(path_image)
+
+# if os.path.exists(path_label):
+#     os.remove(path_label)
+
+# Second: list all directories in the current folder:
+dirlistImg = [item for item in os.listdir(output_image) if os.path.isdir(os.path.join(output_image, item))]
+dirlistLabel = [item for item in os.listdir(output_label) if os.path.isdir(os.path.join(output_label, item))]
+for folder in dirlistImg:
+    print(folder)
+
+print()
+for folder in dirlistLabel:
+    print(folder)
+
+# Third: Move all files listed to correct path
+for folder in dirlistImg:
+    shutil.move(os.path.join(output_image, folder), path_image.replace('class1', '')) # Go to the directorie before class1
+    # Moves folder inside the current folder (which name is 'labels') to inside the current folder in path label
+    print("Arquivo de origem: "+"{}".format(os.path.join(output_label, folder)+"/labels"))
+    print("Arquivo de destino: "+"{}".format(os.path.join(path_label.replace('class2', '/'), folder)))
+    shutil.move((os.path.join(output_label, folder)+"/labels"), os.path.join(path_label.replace('class2', '/'), folder))
+
+# Forth: remove folders 'output' and 'outputlabels'
+# if os.path.exists(output_image):
+#     os.remove(output_image)
+
+# if os.path.exists(output_label):
+#     os.remove(output_label)
