79a90790de7d9035cd9a3af699a091ac86c8f809
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-07-03 11:32:49 -0300

    Dataset com todas as imagens já anotadas add

--- split_data.py
+++ split_data.py
@@ -2,11 +2,11 @@ import splitfolders
 import shutil
 import os
 
-path_image = "Guararema_05_06_23/Third_test/100m+/class1"
-path_label = "Guararema_05_06_23/Third_test/100m+/class2"
+path_image = "Guararema_05_06_23/Third_test/Geral/class1"
+path_label = "Guararema_05_06_23/Third_test/Geral/class2"
 
-output_image = "Guararema_05_06_23/Third_test/100m+/output"
-output_label = "Guararema_05_06_23/Third_test/100m+/outputlabels"
+output_image = "Guararema_05_06_23/Third_test/Geral/output"
+output_label = "Guararema_05_06_23/Third_test/Geral/outputlabels"
 
 # Create folder 'class1' and 'class2'
 if not os.path.exists(path_image):
