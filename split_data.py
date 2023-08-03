2d88566f746d080a6b85f0fd88c5f6bd01368837
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-08-03 12:39:55 -0300

    New dataset to train

--- split_data.py
+++ split_data.py
@@ -6,8 +6,8 @@ import splitfolders
 import shutil
 import os
 
-main_path = "Guararema_e_Arthur/First_test/"   # Path to the folder that will be splited
-split_folder = "Geral/"                         # Folder that will be splited
+main_path = "Guararema_e_Arthur/Third_test/"   # Path to the folder that will be splited
+split_folder = "100m+/"                         # Folder that will be splited
 
 path_image = main_path + split_folder + "class1"
 path_label = main_path + split_folder + "class2"
